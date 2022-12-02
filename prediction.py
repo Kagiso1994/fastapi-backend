02615c6b9bf710eee56e78c0ea89c349572c9998
Kagiso <mokautukagiso@gmail.com>
2022-12-02 19:21:04 +0200

    Commit code

---
+++ prediction.py
@@ -0,0 +1,50 @@
+from PIL import Image
+from io import BytesIO
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Input, Lambda, Dense, Flatten, Conv2D, MaxPool2D
+from keras.models import Model, load_model
+import keras.utils as image
+from keras.applications import imagenet_utils
+import numpy as np
+from keras.applications.resnet import ResNet50, preprocess_input
+from glob import glob
+from datetime import datetime
+import PIL
+
+IMAGE_SIZE = [224,224]
+
+def load_my_model(model_type):
+    model = load_model('./Models/fire_and_smoke2_model.h5')
+    if model_type == "Resnet50":
+        model = load_model('./Models/fire_and_smoke2_model.h5')
+    elif model_type == "VGG16":
+        model = load_model('./Models/vgg16.h5')
+    else:
+        model = load_model('./Models/vgg19.h5')
+    #model.summary()
+    return model
+
+
+#_model = load_my_model()
+
+def read_image(image_encoded):
+    pil_image = image.load_img(BytesIO(image_encoded), target_size = (224,224))
+    pil_image =  image.img_to_array(pil_image)
+    print("Method 2 === ",pil_image)
+    return pil_image
+
+def preprocess(input_image_arr):
+    input_image_arr  = input_image_arr / 255
+    #print("Test after devision", input_image)
+    input_image_arr = np.expand_dims(input_image_arr, axis = 0)
+    return input_image_arr
+
+
+def predict(image: np.ndarray, model_name):
+    _model = load_my_model(model_name)
+    predictions = _model.predict(image)
+    # predictions = imagenet_utils.decode_predictions(predictions)[0][0][1]
+    print("Result == ",predictions)
+    return predictions
+
---
+++ server.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from fastapi import UploadFile,File
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import matplotlib.image as mpimg
+from io import BytesIO
+
+from prediction import predict, preprocess, read_image,load_my_model
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/index')
+def hello_world(name: str):
+    return f"Hello {name}!"
+
+@app.get('/api/model/load')
+def hello_world(model_name: str):
+    return model_name
+
+@app.post('/api/preprocess')
+async def predict_image(model_name: str, file: bytes = File(...)):
+    img = mpimg.imread(BytesIO(file))
+    lum_img = img[:, :, 0]
+    
+    image = preprocess(image)
+    predictions = predict(image, model_name)
+    
+    return "The image classified is Fire" if predictions[0]<0 else "The image classified is Smoke"
+
+
+@app.post('/api/predict')
+async def predict_image(model_name: str, file: bytes = File(...)):
+    image = read_image(file)
+    image = preprocess(image)
+    predictions = predict(image, model_name)
+    
+    return "The image classified is Fire" if predictions[0]<0 else "The image classified is Smoke"
+
+@app.post('/api/predict2')
+async def predict_image(model_name: str):
+    # image = read_image(file)
+    # image = preprocess(image)
+    # predictions = predict(image, model_name)
+    
+    return "The image classified is Fire"
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host='0.0.0.0')
